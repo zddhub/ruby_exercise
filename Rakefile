aacd6758f9441c269967d2c4bd20b827f4cd8f01
zdd <zddhub@gmail.com>
2016-08-16 06:59:03 +0800

    Add test for test_fizz_buzz

--- Rakefile
+++ Rakefile
@@ -1,9 +1,17 @@
 namespace :test do
+  task :fizz_buzz do
+    ruby "test_fizz_buzz.rb"
+  end
+
+  task :auto_wrap do
+    ruby "test_soft_wrap.rb"
+  end
+
   task :numbers_to_words do
     ruby "test_num_to_words.rb"
   end
 
-  task :all => [:numbers_to_words]
+  task :all => [:fizz_buzz, :auto_wrap, :numbers_to_words]
 end
 
 task test: %w[test:all]
---
+++ test_fizz_buzz.rb
@@ -0,0 +1,53 @@
+require "minitest/autorun"
+require './fizz_buzz.rb'
+
+class FizzBuzzTest < Minitest::Test
+  def test_that_should_print_Fizz
+    out, err = capture_io do
+      isFB(3)
+      isFB(13)
+      isFB(301)
+    end
+    assert_equal "Fizz\n"*3, out
+  end
+
+  def test_that_should_print_Buzz
+    out, err = capture_io do
+      isFB(5)
+      isFB(52)
+      isFB(502)
+    end
+    assert_equal "Buzz\n"*3, out
+  end
+
+  def test_that_should_print_Fizz_Buzz
+    out, err = capture_io do
+      isFB(15)
+      isFB(35)
+    end
+    assert_equal "Fizz Buzz\n"*2, out
+  end
+
+  def test_that_should_print_itself
+    out, err = capture_io do
+      isFB(2)
+      isFB(-4)
+    end
+    assert_equal "2\n-4\n", out
+  end
+
+  def test_that_should_not_a_valid_input
+    out, err = capture_io do
+      isFB(3.14)
+      isFB(false)
+      isFB("string")
+    end
+
+    expected_out = <<EOF
+3.14 is not a valid input!
+false is not a valid input!
+string is not a valid input!
+EOF
+    assert_equal expected_out, out
+  end
+end
